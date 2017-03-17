9c45f04d2259e1481d890c136b1c0f3a2ed5e378
Lauren Tan <laurent@netflix.com>
2017-03-17 08:37:00 -0700

    Small refactor

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const winston = require('winston');
+const _object = require('lodash/object');
+const { get } = _object;
+
 const fetchCommit = require('./lib/fetch-commit');
-const processorFn = require('./lib/processor');
+const processDiff = require('./lib/process-diff');
 
 const app = express();
 
@@ -10,15 +14,15 @@ app.set('port', (process.env.PORT || 5000));
 
 app.post('/webhook', function(request, response) {
   let newSha = request.body.after;
-
   if (newSha) {
-    let commitsUrl = request.body.repository.commits_url.replace('{/sha}', `/${newSha}`);
-    fetchCommit(commitsUrl, processorFn);
+    let commitsUrl = get(request, 'body.repository.commits_url', '').replace('{/sha}', `/${newSha}`);
+    fetchCommit(commitsUrl)
+      .then(processDiff)
+      .catch((err) => winston.error(err));
   }
-
   response.send('OK');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  winston.info('Node app is running on port', app.get('port'));
 });
