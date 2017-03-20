32a5824aef8abf454069111454ab20de8c59a0a3
Lauren Tan <laurent@netflix.com>
2017-03-19 18:07:05 -0700

    Verify github signature

--- spec/integration/webhook-test.js
+++ spec/integration/webhook-test.js
@@ -1,26 +1,46 @@
-require('dotenv').config({ path: '../support/.test.env' })
+require('dotenv').config({ path: './spec/support/.test.env' }) // absolute path
 const request = require('supertest');
 const app = require('../../index');
 const webhookMasterPayload = require('../support/fixtures/webhook-master.json');
 const webhookNonMasterPayload = require('../support/fixtures/webhook-non-master.json');
+const generateGithubHeader = require('../support/helpers/generate-github-header');
+const generateSignature = require('../support/helpers/generate-signature');
+
+let headers;
 
 describe('POST /webhook', function() {
   describe('when payload is referencing master', function() {
+    beforeEach(function() {
+      headers = generateGithubHeader({
+        event: 'push',
+        delivery: 'abcd',
+        signature: generateSignature(process.env.GITHUB_WEBHOOK_SECRET, webhookMasterPayload)
+      });
+    });
+
     it('responds with 201', function(done) {
       request(app)
         .post('/webhook')
+        .set(headers)
         .send(webhookMasterPayload)
-        .set('Accept', 'application/json')
         .expect(201, done);
     });
   });
 
   describe('when payload is not referencing master', function() {
+    beforeEach(function() {
+      headers = generateGithubHeader({
+        event: 'push',
+        delivery: 'abcd',
+        signature: generateSignature(process.env.GITHUB_WEBHOOK_SECRET, webhookNonMasterPayload)
+      });
+    });
+
     it('responds with 200', function(done) {
       request(app)
         .post('/webhook')
+        .set(headers)
         .send(webhookNonMasterPayload)
-        .set('Accept', 'application/json')
         .expect(200, done);
     });
   })
